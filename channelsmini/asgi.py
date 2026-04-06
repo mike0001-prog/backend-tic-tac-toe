2017e4a30da52f227cfb5fe50a8e543727b2a655
oyeniyi kehinde <kehindeoyeniyi08@gmail.com>
2026-04-05 17:30:18 -0700

    fixed errors concerning models import in production

--- channelsmini/asgi.py
+++ channelsmini/asgi.py
@@ -7,21 +7,44 @@
 # https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 # """
 
+# import os
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channelsmini.settings')
+
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# import livechat.routing
+# # # from channels.http import AsgiHandler
+# # # from channels.routing import ProtocolTypeRouter
+
+
+
+# application = ProtocolTypeRouter({
+#   "http": get_asgi_application(),
+#   "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             livechat.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channelsmini.settings')
 
+from django.core.asgi import get_asgi_application
+
+# ✅ Initialize Django first
+django_asgi_app = get_asgi_application()
+
+# ✅ Now it's safe to import Channels stuff
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import livechat.routing
-from django.core.asgi import get_asgi_application
-# # from channels.http import AsgiHandler
-# # from channels.routing import ProtocolTypeRouter
-
-
 
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
         URLRouter(
             livechat.routing.websocket_urlpatterns
         )
